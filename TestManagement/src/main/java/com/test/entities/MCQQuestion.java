a86e9c5ad671a6da0e4ef669902c4a799701365f
Vaibhav Shejol <vaibhav.shejol@bnt.local>
2024-06-28 12:13:06 +0530

    MCQQuestion entity updated with subcategory id

--- TestManagement/src/main/java/com/test/entities/MCQQuestion.java
+++ TestManagement/src/main/java/com/test/entities/MCQQuestion.java
@@ -5,62 +5,44 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 @Entity
+@Data
 @Table(name = "mcq_question")
 public class MCQQuestion {
     
     @Id
-    @Setter
-    @Getter
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "question_id")
     private Long id;
 
-    @Setter
-    @Getter
-    private String category;
-
-    @Setter
-    @Getter
     private String question;
 
-    @Setter
-    @Getter
     @Column(name = "option_one")
     private String optionOne;
 
-    @Setter
-    @Getter
     @Column(name = "option_two")
     private String optionTwo;
 
-    @Setter
-    @Getter
     @Column(name = "option_three")
     private String optionThree;
 
-    @Setter
-    @Getter
     @Column(name = "option_four")
     private String optionFour;
 
-    @Setter
-    @Getter
     @Column(name = "correct_option")
     private String correctOption;
 
-    @Setter
-    @Getter
     @Column(name = "positive_mark")
     private int positiveMark;
 
-    @Setter
-    @Getter
     @Column(name = "negative_mark")
     private int negativeMark;
+
+    @JoinColumn(name = "subcategory_id", referencedColumnName = "subcategory_id")
+    private Subcategory subcategory;
 
 }
