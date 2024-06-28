f4842783f80992827445380fb2a1362064bbe34c
Vaibhav Shejol <vaibhav.shejol@bnt.local>
2024-06-28 12:05:36 +0530

    Subcategory entity created

---
+++ TestManagement/src/main/java/com/test/entities/Subcategory.java
@@ -0,0 +1,29 @@
+package com.test.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "subcategory")
+public class Subcategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "subcategory_id")
+    private Long subcategoryId;
+
+    @Column(name = "subcategory_name")
+    private String subcategoryName;
+
+    @Column(name = "subcategory_description")
+    private String subcategoryDescription;
+
+    @JoinColumn(name = "category_id", referencedColumnName = "category_id")
+    private Category category;
+}
