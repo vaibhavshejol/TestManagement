cd06f7d1cc44a7c5f7de97254c7e4d46d334b679
Vaibhav Shejol <vaibhav.shejol@bnt.local>
2024-06-28 19:49:26 +0530

    package problem resolved

--- TestManagement/src/main/java/com/test/service/CategoryServiceImpl.java
+++ TestManagement/src/main/java/com/test/service/CategoryServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.test.entities.Category;
+import com.test.exception.CategoryDeleteException;
 import com.test.repository.CategoryRepository;
 
 @Service
