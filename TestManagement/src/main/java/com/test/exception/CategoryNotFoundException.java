839cbac2cfbbc77884f0de1ab406d68a8172dd5c
Vaibhav Shejol <vaibhav.shejol@bnt.local>
2024-07-04 23:50:17 +0530

    Custom exception class created for category not found

--- TestManagement/src/main/java/com/test/exception/CategoryNotFoundException.java
+++ TestManagement/src/main/java/com/test/exception/CategoryNotFoundException.java
@@ -3,6 +3,7 @@ package com.test.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+//Custom exception class for category not found
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class CategoryNotFoundException extends RuntimeException {
     public CategoryNotFoundException(String message) {
